59e52ef77c5696ae9901e6644c4e46106595b77f
KotaKato <katokota1102@icloud.com>
2019-05-12 19:54:05 +0900

    complete Shiritori.imputWord() method

--- src/shiritori.py
+++ src/shiritori.py
@@ -2,8 +2,8 @@
 import csv
 import MeCab
 
+
 class Shiritori:
-    # dicdata : string(path)
     # プロパティ一覧
     # self.data
     # self.gamemode
@@ -11,55 +11,76 @@ class Shiritori:
     # self.ngWords
     # self.usedWords
     # self.nextChar
+    # dicdata : string(path)
+    kana = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰウヱヲーヴガギグゲゴザジズゼゾダヂヅデドバビブベボヷヸヹヺパピプペポ"
+
     def __init__(self, dictData):
         with open(dictData) as f:
             # 辞書データ
             reader = csv.reader(f)
             self.data = [row for row in reader]
             self.initGame()
+
     def initGame(self):
         # gamemode
-        self.gamemode = 0 # 0 : wait
+        self.gamemode = 0  # 0 : wait
         # mecab
         self.mec = MeCab.Tagger("-Oyomi")
         # NGword
-        self.ngWords = [ "ン" ]
+        self.ngWords = ["ン"]
         # 使われた単語のリスト
         # カタカナ
         self.usedWords = []
         # 次に来る文字
         self.nextChar = ""
+
     def startGame(self):
         #もしgamemodeが0でない(=すでに始まっている)ならError
         if self.gamemode != 0:
             return -1
         self.initGame()
-        self.gamemode = 1 # 1 : player
+        self.gamemode = 0  # 1 : player
+
     def gameOver(self):
         self.gamemode = 0
+
     def nextWord(self):
         # 次の単語を返す
         return "called nextWord()"
+
     def inputWord(self, instring):
         # gamemodeの確認
-        if self.gamemode == 1:
+        if self.gamemode == 2:
             return -1
         # 日本語読みの取得
         # 最後の文字は改行文字になるのでカット
         yomi = self.mec.parse(instring)[:-1]
+        if (yomi == ""):
+            return -2
         # ンで終わる ならば(-1)を返す
         if (yomi[-1] in self.ngWords):
             self.gameOver()
-            return -1
+            return -3
         # すでに使われている
         if (yomi in self.usedWords):
-            return -1
+            return -4
         # 次の文字に合うか
-        if (self.nextChar!="" or yomi[1]!=self.nextChar):
-            return -1
+        if (not self.nextChar == "" and yomi[1] != self.nextChar):
+            return -5
+        # かな表と照らし合わせる
+        if (yomi[-1] not in self.kana):
+            return -6
         # 良いならば
         self.usedWords.append(yomi)
-        self.nextChar = yomi[1]
-        self.gamemode = 2 # Computer
+        self.nextChar = yomi[-1]
+        self.gamemode = 2  # Computer
         print(yomi, self.nextChar)
+        return 0
 
+    def showAllMember(self):
+            # print("self.data : ", self.data)
+            print("self.gamemode : ", self.gamemode)
+            print("self.mec", self.mec)
+            print("self.ngWords", self.ngWords)
+            print("self.usedWords", self.usedWords)
+            print("self.nextchar", self.nextChar)
