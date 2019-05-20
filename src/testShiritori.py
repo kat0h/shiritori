97e493afedd8cf694aa1789d847e2bfaa6ce3760
KotaKato <katokota1102@icloud.com>
2019-05-20 19:25:49 +0900

    しりとりの入力のチェックと反映をする"Shiritori"Classに変更

--- src/testShiritori.py
+++ src/testShiritori.py
@@ -1,17 +1,1 @@
 import shiritori
-
-s = shiritori.Shiritori('./assets/bocchan.csv')
-print("しりとり")
-
-while (s.gamemode != -1):
-    while (True):
-        string = input("your turn ")
-        result = s.inputWord(string)
-        if (result == 0):
-            break
-        else:
-            print("この単語はダメです")
-    print("computer turn ", end="")
-    print(s.nextWord())
-    print(s.usedWords)
-    # s.showAllMember()
